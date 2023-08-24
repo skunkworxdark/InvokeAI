44cb3f72a926d6bf58e0232e77bea2acc7971ded
skunkworxdark <neil_hodgson@hotmail.com>
2023-08-24 18:06:30 +0100

    updated to nodes v1

--- invokeai/app/invocations/images_to_grids.py
+++ invokeai/app/invocations/images_to_grids.py
@@ -1,134 +1,103 @@
-# Copyright (c) 2023 skunkworxdark (https://github.com/skunkworxdark)
+# 2023 skunkworxdark (https://github.com/skunkworxdark)
 
 from typing import Literal, Optional, Union
-
 from PIL import Image, ImageDraw, ImageFont
-from pydantic import BaseModel, Field
 from itertools import product
+
 import json
 import re
 
-from ..models.image import ImageCategory, ImageField, ResourceOrigin, ColorField
-from .baseinvocation import BaseInvocation, BaseInvocationOutput, InvocationContext, InvocationConfig
-from .image import PILInvocationConfig, PIL_RESAMPLING_MODES ,PIL_RESAMPLING_MAP
+from invokeai.app.models.image import ImageCategory, ResourceOrigin
+from invokeai.app.invocations.image import PIL_RESAMPLING_MODES ,PIL_RESAMPLING_MAP
+from invokeai.app.invocations.primitives import (
+    StringOutput,
+    StringCollectionOutput,
+    FloatOutput,
+    IntegerOutput,
+    ColorField,
+    ImageField,
+    ImageCollectionOutput,
+)
+from invokeai.app.invocations.baseinvocation import (
+    BaseInvocation,
+    BaseInvocationOutput,
+    InputField,
+    OutputField,
+    InvocationContext,
+    UIType,
+    tags,
+    title,
+)
 
 
-class FloatsToStringsOutput(BaseInvocationOutput):
-    """FloatsToStringsOutput"""
-    type: Literal["float_to_string_output"] = "float_to_string_output"
-    float_string: list[str] = Field(default=[], description="collection of strings")
-
-    class Config:
-        schema_extra = {"required": ["type", "float_string"]}
-    
+@title("Floats To String")
+@tags("float")
 class FloastToStringsInvocation(BaseInvocation):
     """FloatsToStrings converts a float or collections of floats to a collection of strings"""
     type: Literal["floats_to_strings"] = "floats_to_strings"
-    floats: Union[float, list[float], None] = Field(default=None, description="float or collection of floats")
+    floats: Union[float, list[float], None] = InputField(default=None, description="float or collection of floats", ui_type=UIType.FloatCollection)
 
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "Floats To Strings","type_hints": {"floats": "float"}}}
-
-    def invoke(self, context: InvocationContext) -> FloatsToStringsOutput:
-        """Invoke with provided services and return outputs."""
+    def invoke(self, context: InvocationContext) -> StringCollectionOutput:
         if self.floats is None:
             raise Exception("No collection of floats provided")
         if isinstance(self.floats, list):
-            return FloatsToStringsOutput(float_string=[str(x) for x in self.floats])
+            return StringCollectionOutput(collection=[str(x) for x in self.floats])
         else:
-            return FloatsToStringsOutput(float_string=[str(self.floats)])
+            return StringCollectionOutput(collection=[str(self.floats)])
 
-
-class StringToFloatOutput(BaseInvocationOutput):
-    """StringToFloatOutput"""
-    type: Literal["strings_to_floats_output"] = "strings_to_floats_output"
-    floats: float = Field(default=1.0, description="float")
-
-    class Config:
-        schema_extra = {"required": ["type", "floats"]}
-    
+@title("String To Float")
+@tags("float")
 class StringToFloatInvocation(BaseInvocation):
     """StringToFloat converts a string to a float"""
     type: Literal["string_to_float"] = "string_to_float"
-    float_string: str = Field(default='', description="string")
+    float_string: str = InputField(default='', description="string")
 
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "String To Float"}}
+    def invoke(self, context: InvocationContext) -> FloatOutput:
+        return FloatOutput(value=float(self.float_string))
 
-    def invoke(self, context: InvocationContext) -> StringToFloatOutput:
-        """Invoke with provided services and return outputs."""
-        return StringToFloatOutput(floats=float(self.float_string))
-
-
-class IntsToStringsOutput(BaseInvocationOutput):
-    """IntsToStringsOutput"""
-    type: Literal["ints_to_strings_output"] = "ints_to_strings_output"
-    int_string: list[str] = Field(default=[], description="collection of strings")
-
-    class Config:
-        schema_extra = {"required": ["type", "int_string"]}
-    
+@title("Ints To String")
+@tags("int")
 class IntsToStringsInvocation(BaseInvocation):
     """IntsToStrings converts an int or collection of ints to a collection of strings"""
     type: Literal["ints_to_strings"] = "ints_to_strings"
-    ints: Union[int, list[int], None] = Field(default=None, description="int or collection of ints")
+    ints: Union[int, list[int], None] = InputField(default=None, description="int or collection of ints", ui_type=UIType.IntegerCollection)
 
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "Ints To Strings", "type_hints": {"ints": "integer"}}}
-
-    def invoke(self, context: InvocationContext) -> IntsToStringsOutput:
-        """Invoke with provided services and return outputs."""
+    def invoke(self, context: InvocationContext) -> StringCollectionOutput:
         if self.ints is None:
             raise Exception("No collection of ints provided")
         if isinstance(self.ints, list):
-            return IntsToStringsOutput(int_string=[str(x) for x in self.ints])
+            return StringCollectionOutput(collection=[str(x) for x in self.ints])
         else:
-            return IntsToStringsOutput(int_string=[str(self.ints)])
+            return StringCollectionOutput(collection=[str(self.ints)])
 
-
-class StringToIntOutput(BaseInvocationOutput):
-    """StringToIntOutput"""
-    type: Literal["string_to_int_output"] = "string_to_int_output"
-    ints: int = Field(default=1, description="int")
-
-    class Config:
-        schema_extra = {"required": ["type", "ints"]}
-    
+@title("String To Int")
+@tags("int")       
 class StringToIntInvocation(BaseInvocation):
     """StringToInt converts a string to an int"""
     type: Literal["string_to_int"] = "string_to_int"
-    int_string: str = Field(default='', description="string")
+    int_string: str = InputField(default='', description="string")
 
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "String To Int"}}
-
-    def invoke(self, context: InvocationContext) -> StringToIntOutput:
-        """Invoke with provided services and return outputs."""
-        return StringToIntOutput(ints=int(self.int_string))
+    def invoke(self, context: InvocationContext) -> IntegerOutput:
+        return IntegerOutput(value=int(self.int_string))
 
 
 class XYCollectOutput(BaseInvocationOutput):
     """XYCollectOutput a collection that contains every combination of the input collections"""
     type: Literal["xy_collect_output"] = "xy_collect_output"
-    xy_collection: list[list[str]] = Field(description="The x y product collection")
+    xy_collection: list[list[str]] = OutputField(description="The x y product collection", ui_type=UIType.Collection)
 
-    class Config:
-        schema_extra = {"required": ["type", "xy_collection"]}
-
+@title("XY Collect")
+@tags("xy", "grid", "image")    
 class XYCollectInvocation(BaseInvocation):
     """XYCollect takes two string collections and outputs a collection that every combination of the inputs"""
     type: Literal["xy_collect"] = "xy_collect"
-    x_collection: list[str] = Field(default=[], description="The X collection")
-    y_collection: list[str] = Field(default=[], description="The Y collection")
-
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "XY Collect"}}
+    x_collection: list[str] = InputField(default=[], description="The X collection", ui_type=UIType.StringCollection)
+    y_collection: list[str] = InputField(default=[], description="The Y collection", ui_type=UIType.StringCollection)
 
     def invoke(self, context: InvocationContext) -> XYCollectOutput:
-        """Invoke with provided services and return outputs."""
         return XYCollectOutput(xy_collection=list(product(self.x_collection, self.y_collection)))
 
-# Was going to try have a collection node that could remove the need for a To String Node.
+# Was going to try have a collection node that could remove the need for a To String Nodes.
 # Not working at the moment.
 #
 # class XYCollect2Invocation(BaseInvocation):
@@ -158,78 +127,54 @@ class XYCollectInvocation(BaseInvocation):
 class XYExpandOutput(BaseInvocationOutput):
     """XYExpandOutput two strings that are expanded from a collection of strings"""
     type: Literal["xy_expand_output"] = "xy_expand_output"
-    x_item: str = Field(description="The X item")
-    y_item: str = Field(description="The y item")
+    x_item: str = OutputField(description="The X item")
+    y_item: str = OutputField(description="The y item")
 
-    class Config:
-        schema_extra = {'required': ['type','x_item','y_item']}  
-
+@title("XY Expand")
+@tags("xy", "grid", "image") 
 class XYExpandInvocation(BaseInvocation):
     """XYExpand takes a collection of strings and outputs the first two elements and outputs as individual strings"""
     type: Literal["xy_expand"] = "xy_expand"
-    xy_collection: list[str] = Field(default=[], description="The XY collection item")
-
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "XY Expand"}}
+    xy_collection: list[str] = InputField(default=[], description="The XY collection item", ui_type=UIType.StringCollection)
 
     def invoke(self, context: InvocationContext) -> XYExpandOutput:
-        """Invoke with provided services and return outputs"""
         return XYExpandOutput(x_item=self.xy_collection[0], y_item=self.xy_collection[1])
 
 
-class XYImageCollectOutput(BaseInvocationOutput):
-    """XYImageCollectOutput string containg an array of xItem, Yitem, Image_name converted to json"""
-    type: Literal["xyimage_collect_output"] = "xyimage_collect_output"
-    xyimage: str = Field(description="The XY Image ")
-
-    class Config:
-        schema_extra = {'required': ['type','xyimage']}
-
+@title("XYImage Collect")
+@tags("xy", "grid", "image") 
 class XYImageCollectInvocation(BaseInvocation):
-    """XYImageCollect takes XItem, YItem and an Image and outputs it as an (x_item,y_item,image_name)array converted to json"""
+    """XYImageCollect takes xItem, yItem and an Image and outputs it as an (x_item,y_item,image_name)array converted to json"""
     type: Literal["xyimage_collect"] = "xyimage_collect"
-    x_item: str = Field(default='', description="The X item")
-    y_item: str = Field(default='', description="The Y item")
-    image: ImageField = Field(default=None, description="The image to turn into grids")
+    x_item: str = InputField(default='', description="The X item")
+    y_item: str = InputField(default='', description="The Y item")
+    image: ImageField = InputField(default=None, description="The image to turn into grids")
 
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "XYImage Collect"}}
-
-    def invoke(self, context: InvocationContext) -> XYImageCollectOutput:
-        """Invoke with provided services and return outputs."""
-        return XYImageCollectOutput(xyimage = json.dumps([self.y_item, self.x_item , self.image.image_name]))
+    def invoke(self, context: InvocationContext) -> StringOutput:
+        return StringOutput(value=json.dumps([self.y_item, self.x_item , self.image.image_name]))
 
 
-class XYImageToGridOutput(BaseInvocationOutput):
-    """XYImageToGridOutput collection of image grids generated"""
-    type: Literal["xyimage_grid_output"] = "xyimage_grid_output"
-    collection: list[ImageField] = Field(default=[], description="The output images")
-
-    class Config:
-        schema_extra = {"required": ["type", "collection"]}
-
-class XYImagesToGridInvocation(BaseInvocation):#, PILInvocationConfig):
+@title("XYImage To Grid")
+@tags("xy", "grid", "image") 
+class XYImagesToGridInvocation(BaseInvocation):
     """Load a collection of xyimage types (json of (x_item,y_item,image_name)array) and create a gridimage of them"""
     type: Literal["xyimage_grid"] = "xyimage_grid"
-    xyimages: list[str] = Field(default=[], description="The xyImage Collection")
-    space: int = Field(default=1, ge=0, description="The space to be added between images")
-    scale_factor: Optional[float] = Field(default=1.0, gt=0, description="The factor by which to scale the images")
-    resample_mode:  PIL_RESAMPLING_MODES = Field(default="bicubic", description="The resampling mode")
-    background_color: ColorField = Field(
+    xyimages: list[str] = InputField(default=[], description="The xyImage Collection")
+    space: int = InputField(default=1, ge=0, description="The space to be added between images")
+    scale_factor: Optional[float] = InputField(default=1.0, gt=0, description="The factor by which to scale the images")
+    resample_mode:  PIL_RESAMPLING_MODES = InputField(default="bicubic", description="The resampling mode")
+    background_color: ColorField = InputField(
         default=ColorField(r=0, g=0, b=0, a=255),
         description="The color to use as the background",
     )
-    label_font_name: str = Field(default="arial.ttf", description="Name of the font to use for labels")
-    label_font_size: int = Field(default=35, description="Size of the font to use for labels")
-    top_label_height: int = Field(default=50, description="Height of the top label area")
-    left_label_width: int = Field(default=100, description="Width of the left label area")
-    label_font_color: ColorField = Field(
+    label_font_name: str = InputField(default="arial.ttf", description="Name of the font to use for labels")
+    label_font_size: int = InputField(default=35, description="Size of the font to use for labels")
+    top_label_height: int = InputField(default=50, description="Height of the top label area")
+    left_label_width: int = InputField(default=100, description="Width of the left label area")
+    label_font_color: ColorField = InputField(
         default=ColorField(r=255, g=255, b=255, a=255),
         description="The color to use for the label font",
     )
-
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "XYImage To Grid"}}
     
     def is_all_numeric(self,array):
         pattern = r'^-?\d+(\.\d+)?$'
@@ -252,9 +197,8 @@ class XYImagesToGridInvocation(BaseInvocation):#, PILInvocationConfig):
             return (key0, key1)
         return sorted(arr, key=sort_key2)
 
-    def invoke(self, context: InvocationContext) -> XYImageToGridOutput:
+    def invoke(self, context: InvocationContext) -> ImageCollectionOutput:
         """Convert an image list into a grids of images"""
-
         top_label_space = self.top_label_height
         left_label_space = self.left_label_width
         text_color = self.label_font_color.tuple()
@@ -343,38 +287,27 @@ class XYImagesToGridInvocation(BaseInvocation):#, PILInvocationConfig):
                 is_intermediate=self.is_intermediate,
             )
             grid_images.append(ImageField(image_name=image_dto.image_name))
-        
+
+        return ImageCollectionOutput(collection=grid_images)
 
 
-        return XYImageToGridOutput(collection=grid_images)
-
-
-class ImagesToGridsOutput(BaseInvocationOutput):
-    """ImagesToGridsOutput that output nothing"""
-    type: Literal["image_grid_output"] = "image_grid_output"
-    collection: list[ImageField] = Field(default=[], description="The output images")
-
-    class Config:
-        schema_extra = {"required": ["type", "collection"]}
-
-class ImagesToGridsInvocation(BaseInvocation, PILInvocationConfig):
+@title("Images To Grids")
+@tags("grid", "image") 
+class ImagesToGridsInvocation(BaseInvocation):
     """Load a collection of images and creat grid images from it and output a collection of genereated grid images"""
     type: Literal["image_grid"] = "image_grid"
-    images: list[ImageField] = Field(default=[], description="The image collection to turn into grids")
-    columns: int = Field(default=1, ge=1, description="The number of columns in each grid")
-    rows: int = Field(default=1, ge=1, description="The nuber of rows to have in each grid")
-    space: int = Field(default=1, ge=0, description="The space to be added between images")
-    scale_factor: Optional[float] = Field(default=1.0, gt=0, description="The factor by which to scale the images")
-    resample_mode:  PIL_RESAMPLING_MODES = Field(default="bicubic", description="The resampling mode")
-    background_color: ColorField = Field(
+    images: list[ImageField] = InputField(default=[], description="The image collection to turn into grids")
+    columns: int = InputField(default=1, ge=1, description="The number of columns in each grid")
+    rows: int = InputField(default=1, ge=1, description="The nuber of rows to have in each grid")
+    space: int = InputField(default=1, ge=0, description="The space to be added between images")
+    scale_factor: Optional[float] = InputField(default=1.0, gt=0, description="The factor by which to scale the images")
+    resample_mode:  PIL_RESAMPLING_MODES = InputField(default="bicubic", description="The resampling mode")
+    background_color: ColorField = InputField(
         default=ColorField(r=0, g=0, b=0, a=255),
         description="The color to use as the background",
     )
 
-    class Config(InvocationConfig):
-        schema_extra = {"ui": {"title": "Images To Grids", "type_hints": {"images": "image_collection"}}}
-
-    def invoke(self, context: InvocationContext) -> ImagesToGridsOutput:
+    def invoke(self, context: InvocationContext) -> ImageCollectionOutput:
         """Convert an image list into a grids of images"""
         images = [context.services.images.get_pil_image(image.image_name) for image in self.images]
         width_max = int(max([image.width for image in images]) * self.scale_factor)
@@ -432,4 +365,4 @@ class ImagesToGridsInvocation(BaseInvocation, PILInvocationConfig):
             )
             grid_images.append(ImageField(image_name=image_dto.image_name))
 
-        return ImagesToGridsOutput(collection=grid_images)
+        return ImageCollectionOutput(collection=grid_images)
