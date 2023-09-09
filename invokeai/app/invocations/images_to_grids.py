2bfe04adf7d1a69723647f238f816d8685fcb10e
skunkworxdark <neil_hodgson@hotmail.com>
2023-09-09 19:42:38 +0100

    Updating to 3.1 and polymorpic int and floats

--- invokeai/app/invocations/images_to_grids.py
+++ invokeai/app/invocations/images_to_grids.py
@@ -8,7 +8,7 @@ import json
 import re
 
 from invokeai.app.models.image import ImageCategory, ResourceOrigin
-from invokeai.app.invocations.image import PIL_RESAMPLING_MODES ,PIL_RESAMPLING_MAP
+from invokeai.app.invocations.image import PIL_RESAMPLING_MODES, PIL_RESAMPLING_MAP
 from invokeai.app.invocations.primitives import (
     StringOutput,
     StringCollectionOutput,
@@ -32,10 +32,11 @@ from invokeai.app.invocations.baseinvocation import (
 )
 
 
-@invocation("floats_to_strings", title="Floats To String", tags=["float"], category="util")
-class FloastToStringsInvocation(BaseInvocation):
+@invocation("floats_to_strings", title="Floats To Strings", tags=["float", "string"], category="util", version="1.0.0")
+class FloatsToStringsInvocation(BaseInvocation):
     """FloatsToStrings converts a float or collections of floats to a collection of strings"""
-    floats: Union[float, list[float], None] = InputField(default=None, description="float or collection of floats", ui_type=UIType.Collection, input=Input.Connection)
+
+    floats: Union[float, list[float]] = InputField(default_factory=list, description="float or collection of floats")
 
     def invoke(self, context: InvocationContext) -> StringCollectionOutput:
         if self.floats is None:
@@ -46,19 +47,21 @@ class FloastToStringsInvocation(BaseInvocation):
             return StringCollectionOutput(collection=[str(self.floats)])
 
 
-@invocation("string_to_float", title="String To Float", tags=["float"], category="util")
+@invocation("string_to_float", title="String To Float", tags=["float", "string"], category="util", version="1.0.0")
 class StringToFloatInvocation(BaseInvocation):
     """StringToFloat converts a string to a float"""
-    float_string: str = InputField(default='', description="string containg a float to convert")
+
+    float_string: str = InputField(description="string containg a float to convert")
 
     def invoke(self, context: InvocationContext) -> FloatOutput:
         return FloatOutput(value=float(self.float_string))
 
 
-@invocation("ints_to_strings", title="Ints To String", tags=["int"], category="util")
+@invocation("ints_to_strings", title="Ints To String", tags=["int", "string"], category="util", version="1.0.0")
 class IntsToStringsInvocation(BaseInvocation):
     """IntsToStrings converts an int or collection of ints to a collection of strings"""
-    ints: Union[int, list[int], None] = InputField(default=None, description="int or collection of ints", ui_type=UIType.Collection, input=Input.Connection)
+
+    ints: Union[int, list[int]] = InputField(default_factory=list, description="int or collection of ints")
 
     def invoke(self, context: InvocationContext) -> StringCollectionOutput:
         if self.ints is None:
@@ -68,11 +71,12 @@ class IntsToStringsInvocation(BaseInvocation):
         else:
             return StringCollectionOutput(collection=[str(self.ints)])
 
-   
-@invocation("string_to_int", title="String To Int", tags=["int"], category="util")
+
+@invocation("string_to_int", title="String To Int", tags=["int"], category="util", version="1.0.0")
 class StringToIntInvocation(BaseInvocation):
     """StringToInt converts a string to an int"""
-    int_string: str = InputField(default='', description="string containing an integer to convert")
+
+    int_string: str = InputField(description="string containing an integer to convert")
 
     def invoke(self, context: InvocationContext) -> IntegerOutput:
         return IntegerOutput(value=int(self.int_string))
@@ -81,33 +85,39 @@ class StringToIntInvocation(BaseInvocation):
 @invocation_output("xy_collect_output")
 class XYCollectOutput(BaseInvocationOutput):
     """XYCollectOutput a collection that contains every combination of the input collections"""
+
     xy_collection: list[list[str]] = OutputField(description="The x y product collection", ui_type=UIType.Collection)
 
 
-@invocation("xy_collect", title="XY Collect", tags=["xy", "grid", "collect"], category="grid")
+@invocation("xy_collect", title="XY Collect", tags=["xy", "grid", "collect"], category="grid", version="1.0.0")
 class XYCollectInvocation(BaseInvocation):
     """XYCollect takes two string collections and outputs a collection that every combination of the inputs"""
-    x_collection: list[str] = InputField(default=[], description="The X collection", ui_type=UIType.Collection, input=Input.Connection)
-    y_collection: list[str] = InputField(default=[], description="The Y collection", ui_type=UIType.Collection, input=Input.Connection)
+
+    x_collection: list[str] = InputField(default_factory=list, description="The X collection")
+    y_collection: list[str] = InputField(default_factory=list, description="The Y collection")
 
     def invoke(self, context: InvocationContext) -> XYCollectOutput:
         return XYCollectOutput(xy_collection=list(product(self.x_collection, self.y_collection)))
 
 
-@invocation("xy_csv_to_strings", title="XY CSV to Strings", tags=["xy", "grid", "csv"], category="grid")
+@invocation(
+    "xy_csv_to_strings", title="XY CSV to Strings", tags=["xy", "grid", "csv"], category="grid", version="1.0.0"
+)
 class XYCSVToStringsInvocation(BaseInvocation):
     """XYCSVToStrings converts X and Y CSV Strings to a collection that every combination of X and Y"""
-    x: str = InputField(default='', description="x string", ui_component=UIComponent.Textarea)
-    y: str = InputField(default='', description="y string", ui_component=UIComponent.Textarea)
+
+    x: str = InputField(description="x string", ui_component=UIComponent.Textarea)
+    y: str = InputField(description="y string", ui_component=UIComponent.Textarea)
 
     def invoke(self, context: InvocationContext) -> XYCollectOutput:
         return XYCollectOutput(xy_collection=list(product(self.x.split(","), self.y.split(","))))
 
 
-@invocation("csv_to_strings", title="CSV To Strings", tags=["xy", "grid", "csv"], category="grid")
+@invocation("csv_to_strings", title="CSV To Strings", tags=["xy", "grid", "csv"], category="grid", version="1.0.0")
 class CSVToStringsInvocation(BaseInvocation):
     """CSVToStrings converts a CSV String to a collection of strings"""
-    csv: str = InputField(default='', description="csv string")
+
+    csv: str = InputField(description="csv string")
 
     def invoke(self, context: InvocationContext) -> StringCollectionOutput:
         return StringCollectionOutput(collection=self.csv.split(","))
@@ -116,36 +126,45 @@ class CSVToStringsInvocation(BaseInvocation):
 @invocation_output("xy_expand_output")
 class XYExpandOutput(BaseInvocationOutput):
     """XYExpandOutput two strings that are expanded from a collection of strings"""
+
     x_item: str = OutputField(description="The X item")
     y_item: str = OutputField(description="The y item")
 
-@invocation("xy_expand", title="XY Expand", tags=["xy", "grid"], category="grid")
+
+@invocation("xy_expand", title="XY Expand", tags=["xy", "grid"], category="grid", version="1.0.0")
 class XYExpandInvocation(BaseInvocation):
     """XYExpand takes a collection of strings and outputs the first two elements and outputs as individual strings"""
-    xy_collection: list[str] = InputField(default=[], description="The XY collection item", ui_type=UIType.Collection, input=Input.Connection)
+
+    xy_item: list[str] = InputField(default_factory=list, description="The XY collection item")
 
     def invoke(self, context: InvocationContext) -> XYExpandOutput:
         return XYExpandOutput(x_item=self.xy_collection[0], y_item=self.xy_collection[1])
 
 
-@invocation("xyimage_collect", title="XYImage Collect", tags=["xy", "grid", "image"], category="grid")
+@invocation("xyimage_collect", title="XYImage Collect", tags=["xy", "grid", "image"], category="grid", version="1.0.0")
 class XYImageCollectInvocation(BaseInvocation):
     """XYImageCollect takes xItem, yItem and an Image and outputs it as an (x_item,y_item,image_name)array converted to json"""
-    x_item: str = InputField(default='', description="The X item", input=Input.Connection)
-    y_item: str = InputField(default='', description="The Y item", input=Input.Connection)
-    image: ImageField = InputField(default=None, description="The image to turn into grids", input=Input.Connection)
+
+    x_item: str = InputField(description="The X item")
+    y_item: str = InputField(description="The Y item")
+    image: ImageField = InputField(description="The image to turn into grids")
 
     def invoke(self, context: InvocationContext) -> StringOutput:
-        return StringOutput(value=json.dumps([self.y_item, self.x_item , self.image.image_name]))
+        return StringOutput(value=json.dumps([self.y_item, self.x_item, self.image.image_name]))
 
 
-@invocation("xyimage_grid", title="XYImage To Grid", tags=["xy", "grid", "image"], category="grid")
+@invocation("xyimage_grid", title="XYImage To Grid", tags=["xy", "grid", "image"], category="grid", version="1.0.0")
 class XYImagesToGridInvocation(BaseInvocation):
     """Load a collection of xyimage types (json of (x_item,y_item,image_name)array) and create a gridimage of them"""
-    xyimages: list[str] = InputField(default=[], description="The xyImage Collection", ui_type=UIType.Collection, input=Input.Connection)
+
+    xyimages: list[str] = InputField(
+        default_factory=list,
+        description="The xyImage Collection",
+        ui_type=UIType.Collection,
+    )
     space: int = InputField(default=1, ge=0, description="The space to be added between images")
     scale_factor: Optional[float] = InputField(default=1.0, gt=0, description="The factor by which to scale the images")
-    resample_mode:  PIL_RESAMPLING_MODES = InputField(default="bicubic", description="The resampling mode")
+    resample_mode: PIL_RESAMPLING_MODES = InputField(default="bicubic", description="The resampling mode")
     background_color: ColorField = InputField(
         default=ColorField(r=0, g=0, b=0, a=255),
         description="The color to use as the background",
@@ -158,26 +177,28 @@ class XYImagesToGridInvocation(BaseInvocation):
         default=ColorField(r=255, g=255, b=255, a=255),
         description="The color to use for the label font",
     )
-    
-    def is_all_numeric(self,array):
-        pattern = r'^-?\d+(\.\d+)?$'
+
+    def is_all_numeric(self, array):
+        pattern = r"^-?\d+(\.\d+)?$"
         return all(re.match(pattern, item) for item in array)
 
     def sort_array(self, arr):
         def sort_key(x):
             key0 = float(x) if self.is_all_numeric(arr) else x[0]
-            return (key0)
+            return key0
+
         return sorted(arr, key=sort_key)
-    
-    def is_all_numeric2(self,array, i):
-        pattern = r'^-?\d+(\.\d+)?$'
+
+    def is_all_numeric2(self, array, i):
+        pattern = r"^-?\d+(\.\d+)?$"
         return all(re.match(pattern, item[i]) for item in array)
 
     def sort_array2(self, arr):
         def sort_key2(x):
-            key0 = float(x[0]) if self.is_all_numeric2(arr,0) else x[0]
-            key1 = float(x[1]) if self.is_all_numeric2(arr,1) else x[1]
+            key0 = float(x[0]) if self.is_all_numeric2(arr, 0) else x[0]
+            key1 = float(x[1]) if self.is_all_numeric2(arr, 1) else x[1]
             return (key0, key1)
+
         return sorted(arr, key=sort_key2)
 
     def invoke(self, context: InvocationContext) -> ImageCollectionOutput:
@@ -185,7 +206,7 @@ class XYImagesToGridInvocation(BaseInvocation):
         top_label_space = self.top_label_height
         left_label_space = self.left_label_width
         text_color = self.label_font_color.tuple()
-        font =  ImageFont.truetype(self.label_font_name, self.label_font_size)
+        font = ImageFont.truetype(self.label_font_name, self.label_font_size)
 
         new_array = [json.loads(s) for s in self.xyimages]
         sorted_array = self.sort_array2(new_array)
@@ -204,7 +225,7 @@ class XYImagesToGridInvocation(BaseInvocation):
         row = 0
         x_offset = left_label_space
         y_offset = top_label_space
-        background = Image.new('RGBA', (background_width, background_height), self.background_color.tuple())
+        background = Image.new("RGBA", (background_width, background_height), self.background_color.tuple())
         grid_images = []
 
         for image in images:
@@ -223,14 +244,15 @@ class XYImagesToGridInvocation(BaseInvocation):
                 x_offset = left_label_space
                 y_offset += height_max + self.space
                 row += 1
-            
+
+            # only draw the labels if we have done the last row
             if row >= rows:
                 row = 0
                 y_offset = top_label_space
 
                 draw = ImageDraw.Draw(background)
                 labelx = left_label_space
-                labely =  0 
+                labely = 0
                 for label in column_names:
                     text_bbox = font.getbbox(label)
                     text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[0]
@@ -258,9 +280,10 @@ class XYImagesToGridInvocation(BaseInvocation):
                     is_intermediate=self.is_intermediate,
                 )
                 grid_images.append(ImageField(image_name=image_dto.image_name))
-                background = Image.new('RGBA', (background_width, background_height), self.background_color.tuple())
-        
-        if column > 0 or row > 0:
+                background = Image.new("RGBA", (background_width, background_height), self.background_color.tuple())
+
+        # if we are not on column and row 0 then we have a part done grid and neew to save it
+        if column > 0 and row > 0:
             image_dto = context.services.images.create(
                 image=background,
                 image_origin=ResourceOrigin.INTERNAL,
@@ -274,15 +297,20 @@ class XYImagesToGridInvocation(BaseInvocation):
         return ImageCollectionOutput(collection=grid_images)
 
 
-@invocation("image_grid", title="Images To Grids", tags=["grid", "image"], category="grid")
+@invocation("image_grid", title="Images To Grids", tags=["grid", "image"], category="grid", version="1.0.0")
 class ImagesToGridsInvocation(BaseInvocation):
     """Load a collection of images and creat grid images from it and output a collection of genereated grid images"""
-    images: list[ImageField] = InputField(default=[], description="The image collection to turn into grids", ui_type=UIType.ImageCollection, input=Input.Connection)
+
+    images: list[ImageField] = InputField(
+        default_factory=list,
+        description="The image collection to turn into grids",
+        ui_type=UIType.ImageCollection,
+    )
     columns: int = InputField(default=1, ge=1, description="The number of columns in each grid")
     rows: int = InputField(default=1, ge=1, description="The nuber of rows to have in each grid")
     space: int = InputField(default=1, ge=0, description="The space to be added between images")
     scale_factor: Optional[float] = InputField(default=1.0, gt=0, description="The factor by which to scale the images")
-    resample_mode:  PIL_RESAMPLING_MODES = InputField(default="bicubic", description="The resampling mode")
+    resample_mode: PIL_RESAMPLING_MODES = InputField(default="bicubic", description="The resampling mode")
     background_color: ColorField = InputField(
         default=ColorField(r=0, g=0, b=0, a=255),
         description="The color to use as the background",
@@ -301,7 +329,7 @@ class ImagesToGridsInvocation(BaseInvocation):
         row = 0
         x_offset = 0
         y_offset = 0
-        background = Image.new('RGBA', (background_width, background_height), self.background_color.tuple())
+        background = Image.new("RGBA", (background_width, background_height), self.background_color.tuple())
         grid_images = []
 
         for image in images:
@@ -320,7 +348,7 @@ class ImagesToGridsInvocation(BaseInvocation):
                 x_offset = 0
                 y_offset += height_max + self.space
                 row += 1
-            
+
             if row >= self.rows:
                 row = 0
                 y_offset = 0
@@ -333,8 +361,9 @@ class ImagesToGridsInvocation(BaseInvocation):
                     is_intermediate=self.is_intermediate,
                 )
                 grid_images.append(ImageField(image_name=image_dto.image_name))
-                background = Image.new('RGBA', (background_width, background_height), self.background_color.tuple())
-        
+                background = Image.new("RGBA", (background_width, background_height), self.background_color.tuple())
+
+        # if we are not on column and row 0 then we have a part done grid and neew to save it
         if column > 0 or row > 0:
             image_dto = context.services.images.create(
                 image=background,
